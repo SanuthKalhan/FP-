d042ca6cc747ca1388b781a73b1b2a18c02aa8da
Mr. S.K.WANNIARACHCHI <136556853+SanuthKalhan@users.noreply.github.com>
2023-06-23 10:00:26 +0530

    Update 21002088_Q4.scala

--- 21002088 - Tutorial 2/21002088_Q4.scala
+++ 21002088 - Tutorial 2/21002088_Q4.scala
@@ -3,21 +3,8 @@ package com.sanuth.scala
 object demo5 extends App {
   def profit(price:Int,att:Int)=att*price-(500+att*3)
 
-  def priceAnalyse(): Int ={
-    var price = 5
-    var attendance = 160
-    var max = profit(price,attendance)
-    price+=5
-    attendance-=20
-    while(max<profit(price,attendance)){
-      max = profit(price,attendance)
-      //println(max,price)
-      price+=5
-      attendance-=20
-    }
-    price-=5
-    price
-  }
+  def priceAnalyse(price:Int,att:Int): Int = if(profit(price,att)<profit(price+5,att-20)) priceAnalyse(price+5,att-20) else price
 
-  println(s"Price : ${priceAnalyse()}")
+
+  println(s"Price : ${priceAnalyse(5,160)}")
 }
