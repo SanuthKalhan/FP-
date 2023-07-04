d969cad258c8250f9fbcfa7f31f7d0653dd6d93a
Mr. S.K.WANNIARACHCHI <136556853+SanuthKalhan@users.noreply.github.com>
2023-07-04 23:29:47 +0530

    Update Q3.scala

--- 21002088- Tutorial 4/Q3.scala
+++ 21002088- Tutorial 4/Q3.scala
@@ -3,15 +3,22 @@ package com.sanuth.scala
 object demo13 extends App {
   def toUpper(str:String)=str.toUpperCase()
   def toLower(str:String)=str.toLowerCase()
+  def funcForFirst(str:String)=str.toUpperCase()
+  def funcForSec(str:String)={
+    str.substring(0,2).toUpperCase() + str.substring(2).toLowerCase()
+  }
+  def funcForThird(str:String)=str.toLowerCase()
+  def funcForFourth(str:String)={
+    str.substring(0,1).toUpperCase()+str.substring(1,str.length()-1)+str.substring(str.length()-1).toUpperCase()
+  }
 
-  def formatNames(name:String,formatFunc1:String=>String,formatFunc2:String=>String): Unit ={
-    if(name.length()%2==0){
-      println(s"${formatFunc1(name)}")
-    }else{
-      println(s"${formatFunc2(name)}")
-    }
+  def formatNames(name:String,formatFunc1:String=>String): String ={
+    formatFunc1(name)
   }
 
   val names = List("Benny","Niroshan","Saman","Kumara")
-  names.foreach(name => formatNames(name,toLower,toUpper))
+  println(s"${formatNames(names(0),funcForFirst)}")
+  println(s"${formatNames(names(1),funcForSec)}")
+  println(s"${formatNames(names(2),funcForThird)}")
+  println(s"${formatNames(names(3),funcForFourth)}")
 }
