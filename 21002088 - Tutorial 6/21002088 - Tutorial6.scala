d24d62478b8244967a4f197d9f16d61467aac391
Mr. S.K.WANNIARACHCHI <136556853+SanuthKalhan@users.noreply.github.com>
2023-07-23 21:58:02 +0530

    Add files via upload

---
+++ 21002088 - Tutorial 6/21002088 - Tutorial6.scala
@@ -0,0 +1,51 @@
+package com.sanuth.scala
+
+object demo21 extends App {
+  def encryptEngine(word:String):String={
+    var num:Int = 1
+    var newWord:String=""
+    word.foreach{char=>
+      var temp = char.toInt + num
+      num = num + 1
+      newWord = newWord + temp.toChar.toString
+    }
+    newWord
+  }
+
+  def encrypt(str:String):String={
+    var strArray:Array[String] = str.split(" ")
+    var encString=""
+    for(word <- strArray){
+      var encWord:String = encryptEngine(word)
+      encString = encString + " " + encWord
+    }
+    encString
+  }
+
+  def decryptEngine(word:String):String={
+    var num:Int = 1
+    var original:String=""
+    word.foreach {char =>
+      var temp = char.toInt - num
+      num = num + 1
+      original = original + temp.toChar.toString
+    }
+    original
+  }
+
+  def decrypt(str:String):String={
+    var strArray:Array[String] = str.split(" ")
+    var decString=""
+    for(word <- strArray){
+      var decWord:String = decryptEngine(word)
+      decString = decString + " " + decWord
+    }
+    decString
+  }
+
+  var encW = encrypt("This String is Encrypted")
+  println(s"Encrypted String : ${encW}")
+  println(s"Decrypted String :${decrypt(encW)}")
+
+
+}
