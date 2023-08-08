b2fd58fe7f36fe477e6414e8cb7eeac0aa6c1cd9
Mr. S.K.WANNIARACHCHI <136556853+SanuthKalhan@users.noreply.github.com>
2023-08-08 23:14:54 +0530

    Update 21002088 - Q2.scala

--- 21002088 - Tutorial 07/21002088 - Q2.scala
+++ 21002088 - Tutorial 07/21002088 - Q2.scala
@@ -1,5 +1,12 @@
 package com.sanuth.scala
 
 object demo25 extends App {
-  //Q2
+  def calculateSquare(numbers: List[Int]): List[Int] = {
+    numbers.map(num => num * num)
+  }
+
+  val input = List(1, 2, 3, 4, 5)
+  val output = calculateSquare(input)
+  println(output)
+
 }
